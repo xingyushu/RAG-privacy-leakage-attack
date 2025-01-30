8a71c3419eeb70fbbd875f3a200ce49a50c638e7
xingyushu <xingyulv@foxmail.com>
2025-01-30 00:42:04 +0000

    fix the error of the offical one

---
+++ wiki-target.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node=1 --master_port=27000 run_language_model.py --ckpt_dir llama-2-7b-chat --temperature 0.6 --top_p 0.9 --max_seq_len 4096 --max_gen_len 256 --path "wiki-target/Q-R-T-" ;
